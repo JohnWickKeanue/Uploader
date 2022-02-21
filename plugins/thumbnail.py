b7ccb0b6f94aaaa5543c12123a868e3e3fb030d3
JohnWickKeanue <92579700+JohnWickKeanue@users.noreply.github.com>
2022-02-21 20:02:18 +0530

    Add files via upload

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -9,7 +9,7 @@ import numpy
 import os
 from PIL import Image
 import time
-
+from .database.add import add_user_to_database
 # the Strings used for this "thing"
 from plugins.main import Translation
 from pyrogram import Client
@@ -21,30 +21,44 @@ from pyrogram import filters
 from .database.database import db
 from .functions.help_Nekmo_ffmpeg import take_screen_shot
 
-@Client.on_message(filters.private & filters.photo)
-async def save_photo(bot, update):
-    
-    await db.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+@Client.on_message(filters.command("setthumb") & filters.private & ~filters.edited)
+async def set_thumbnail(c: Client, m: "types.Message"):
+    if (not m.reply_to_message) or (not m.reply_to_message.photo):
+        return await m.reply_text("Reply to any image to save in as custom thumbnail!")
+    if not m.from_user:
+        return await m.reply_text("I don't know about you sar :(")
+    await add_user_to_database(c, m)
+    await db.set_thumbnail(m.from_user.id, m.reply_to_message.photo.file_id)
+    await m.reply_text("Okay,\n"
+                       "I will use this image as custom thumbnail.",
+                       reply_markup=types.InlineKeyboardMarkup(
+                           [[types.InlineKeyboardButton("Delete Thumbnail",
+                                                        callback_data="deleteThumbnail")]]
+                       ))
 
-@Client.on_message(filters.private & filters.command("delthumb"))
-async def delthumbnail(bot, update):
-    
-    await db.set_thumbnail(update.from_user.id, thumbnail=None)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+@Client.on_message(filters.command("delthumb") & filters.private & ~filters.edited)
+async def delete_thumbnail(c: Client, m: "types.Message"):
+    if not m.from_user:
+        return await m.reply_text("I don't know about you sar :(")
+    await add_user_to_database(c, m)
+    await db.set_thumbnail(m.from_user.id, None)
+    await m.reply_text("Okay,\n"
+                       "I deleted custom thumbnail from my database.")
 
-@Client.on_message(filters.private & filters.command("showthumb") )
+@Client.on_message(filters.private & filters.command("sthumb") )
 async def viewthumbnail(bot, update):
-    
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
     thumbnail = await db.get_thumbnail(update.from_user.id)
-    if thumbnail is not None:
-        await bot.send_photo(
-        chat_id=update.chat.id,
-        photo=thumbnail,
-        caption=f"Yᴏᴜʀ ᴄᴜʀʀᴇɴᴛ sᴀᴠᴇᴅ ᴛʜᴜᴍʙɴᴀɪʟ 🦠",
-        reply_to_message_id=update.message_id)
-    else:
-        await update.reply_text(text=f"Nᴏ Tʜᴜᴍʙɴᴀɪʟ ғᴏᴜɴᴅ 🤒")
+    if not thumbnail:
+        return await update.reply_text("You didn't set custom thumbnail!")
+    await bot.send_photo(update.chat.id, thumbnail, caption="Custom Thumbnail",
+                       reply_markup=types.InlineKeyboardMarkup(
+                           [[types.InlineKeyboardButton("Delete Thumbnail",
+                                                        callback_data="deleteThumbnail")]]
+                       ))
+
 
 async def Gthumb01(bot, update):
     thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
